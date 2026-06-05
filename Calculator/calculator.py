5591758ea61ade6cf4dcd678f166ab885101f6ef
anshika16singh12-cloud <anshika16singh12@gmail.com>
2026-06-05 15:33:15 +0530

    Add calculator program

---
+++ Calculator/calculator.py
@@ -0,0 +1,27 @@
+# MAKE A CALCULATOR
+print("Addition")
+print("Subtraction")
+print("Multiplication")
+print("Division")
+option = int(input("Choose an operation:"))
+result = 0
+if (option in [1,2,3,4]):
+    num1 = int(input("Enter first number:"))
+    num2 = int(input("Enter second number:"))
+
+    if(option == 1):
+        result = num1 + num2
+
+    elif(option == 2):
+        result = num1 - num2
+
+    elif(option == 3):
+        result = num1 * num2
+
+    elif(option == 4):
+        result = num1 // num2
+
+else:
+    print("Invalid operation entered")
+
+print("The result of the operation is {}".format(result))
